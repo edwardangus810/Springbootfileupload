e39b8c57039e3be47818cc5fd4367894cd14cda2
edwaardangus810 <3448188381@qq.com>
2020-06-13 19:28:58 +0800

    SpringBoot文件上传

---
+++ src/main/java/com/example/springbootfileupload/SpringbootfileuploadApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfileuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfileuploadApplication.class, args);
+	}
+
+}
